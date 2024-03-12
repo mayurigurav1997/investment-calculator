e217f26e62a5bfb8f99f2be736c936675740350f
Mayuri <mayurigurav1997@gmail.com>
2024-03-12 22:58:24 +0530

    handling number value

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { calculateInvestmentResults } from '../util/investment'
 
 const Results = ({ input }) => {
+    const resultData = calculateInvestmentResults(input)
     return (
         <div>
 
